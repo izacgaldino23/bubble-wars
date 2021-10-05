9f8e495fe9af9d090e9114cc155ec81743faa995
izacgaldino23 <izacgaldino@outlook.com>
2021-10-04 21:33:14 -0300

    Atualização de animação do tentaculo e das bolhinhas

--- js/sketch.js
+++ js/sketch.js
@@ -14,14 +14,15 @@ const maxs = [ 15, 35, 125 ] // Maximos predefinidos
 const prod = [ 100, 80, 60 ] // Ticker de produção por tier 
 
 const circlePathSize = 15
-const maxAnim = 100
+const maxAnim = 50
+const pulseBall = 3
 
 const test_scene = {
 	towers: [
 		{ team: TEAM_3, tier: TIER_1, power: 8, pos: { x: 100, y: 100 } },
 		{ team: TEAM_1, tier: TIER_2, power: 25, pos: { x: 300, y: 400 } },
 		{ team: TEAM_2, tier: TIER_3, power: 80, pos: { x: 500, y: 300 } },
-		{ team: TEAM_3, tier: TIER_2, power: 120, pos: { x: 300, y: 600 } },
+		{ team: TEAM_4, tier: TIER_2, power: 120, pos: { x: 300, y: 600 } },
 	]
 }
 
@@ -179,7 +180,7 @@ class Tower {
 
 		if (this.tick >= prod[ this.tier ]) {
 			this.tick = 0
-			this.power++
+			if (this.power < this.max) this.power++
 		} else this.tick++
 	}
 
@@ -203,13 +204,19 @@ class Path {
 		this.tower2 = tower2
 		this.balls = []
 
-		let middleX = (tower1.pos.x + tower2.pos.x) / 2
-		let middleY = (tower1.pos.y + tower2.pos.y) / 2
+		this.angleT1 = atan2(this.tower2.pos.y - this.tower1.pos.y, this.tower2.pos.x - this.tower1.pos.x)
+		this.angleT2 = atan2(this.tower1.pos.y - this.tower2.pos.y, this.tower1.pos.x - this.tower2.pos.x)
+
+		this.pointT1 = createVector(this.tower1.pos.x + (this.tower1.r / 2) * cos(this.angleT1), this.tower1.pos.y + (this.tower1.r / 2) * sin(this.angleT1))
+		this.pointT2 = createVector(this.tower2.pos.x + (this.tower2.r / 2) * cos(this.angleT2), this.tower2.pos.y + (this.tower2.r / 2) * sin(this.angleT2))
+
+		let middleX = (this.pointT1.x + this.pointT2.x) / 2
+		let middleY = (this.pointT1.y + this.pointT2.y) / 2
 
 		this.middle = createVector(middleX, middleY)
-		this.size = dist(tower1.pos.x, tower1.pos.y, tower2.pos.x, tower2.pos.y)// - (tower1.r / 2) - (tower2.r / 2)
+		this.size = dist(this.pointT1.x, this.pointT1.y, this.pointT2.x, this.pointT2.y)
 
-		this.steps = floor(this.size / (circlePathSize / 0.7))
+		this.steps = floor(this.size / (circlePathSize * 1.5))
 
 		tower1.paths.push(tower2.id)
 		tower2.paths.push(tower1.id)
@@ -222,6 +229,11 @@ class Path {
 		for (let i in this.balls) {
 			this.balls[ i ].draw()
 		}
+
+		stroke(255)
+		noFill()
+		// circle(this.pointT1.x, this.pointT1.y, 20)
+		// circle(this.pointT2.x, this.pointT2.y, 20)
 	}
 
 	generateBalls () {
@@ -229,20 +241,24 @@ class Path {
 
 		for (let i = 0; i <= this.steps; i++) {
 			let m = map(i, 0, this.steps, 0, 1)
-			let x = lerp(this.tower1.pos.x, this.tower2.pos.x, m)
-			let y = lerp(this.tower1.pos.y, this.tower2.pos.y, m)
+			let x = lerp(this.pointT1.x, this.pointT2.x, m)
+			let y = lerp(this.pointT1.y, this.pointT2.y, m)
 			let newColor = lerpColor(this.tower1.color, this.tower2.color, m)
 
 			let temp = new PathBall(
 				x,
 				y,
-				newColor,
+				color(colors[ this.tower1.team ]),// newColor,
 				this,
 				atan2(this.tower2.pos.y - this.tower1.pos.y, this.tower2.pos.x - this.tower1.pos.x),
-				i * 4
+				i * 4,
+				(m <= 0.5 ? m : 0.5 - (m - 0.5)) / 0.5,
 			)
 			if (i == middle) {
 				temp.center = true
+			}
+			if (i == 0 || i == this.steps) {
+				temp.fixed = true
 			}
 
 			this.balls.push(temp)
@@ -251,11 +267,12 @@ class Path {
 }
 
 class PathBall {
-	constructor(x, y, color, path, angle, i) {
+	constructor(x, y, color, path, angle, i, forceAnim) {
 		this.pos = createVector(x, y)
 		this.color = color
 		this.path = path
 		this.center = false
+		this.fixed = false
 
 		this.angle = angle + PI / 2
 
@@ -266,7 +283,11 @@ class PathBall {
 		this.delay = i
 		this.alpha = 0
 
-		this.posAnim1 = createVector(cos(this.angle), sin(this.angle)).mult(.1)
+		this.pulseFrame = floor(map(forceAnim, 0, 1, pulseBall + 1, maxAnim))
+		this.plusPulse = 0
+		this.pulsing = false
+
+		this.posAnim1 = createVector(cos(this.angle), sin(this.angle)).mult(0.05)
 		this.color.setAlpha(0)
 
 		this.id = pathBallID
@@ -282,7 +303,7 @@ class PathBall {
 		noStroke()
 		fill(this.color)
 
-		circle(this.pos.x, this.pos.y, circlePathSize)
+		circle(this.pos.x, this.pos.y, circlePathSize + this.plusPulse)
 
 		this.update()
 	}
@@ -296,13 +317,21 @@ class PathBall {
 			this.direction = 1
 		}
 
+		this.pulsing = this.frame <= pulseBall
+		this.plusPulse += (this.pulsing ? 1 : -1)
+		if (this.plusPulse < 0) this.plusPulse = 0
+
 		this.frame += this.direction
+		this.pulseFrame += this.direction
+
 		if (this.alpha < 255) {
-			this.alpha += 6
+			this.alpha += 10
 			this.color.setAlpha(this.alpha)
 		}
 
-		if (this.up) this.pos.add(this.posAnim1)
-		else this.pos.sub(this.posAnim1)
+		if (!this.fixed) {
+			if (this.up) this.pos.add(this.posAnim1)
+			else this.pos.sub(this.posAnim1)
+		}
 	}
 }
